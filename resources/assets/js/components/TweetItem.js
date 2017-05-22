d822d91c57087c93aa12c8dda26edd3fb31ab88a
Samantha Geitz <samanthamichele7@gmail.com>
2017-05-22 11:33:01 -0500

    Hard-coded profile pictures

--- resources/assets/js/components/TweetItem.js
+++ resources/assets/js/components/TweetItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment';
-import { ListGroupItem } from 'react-bootstrap';
+import { ListGroupItem, Image } from 'react-bootstrap';
 
 export default ({ deleteTweet, tweet }) => {
     const trashButtonStyle = {
@@ -10,15 +10,38 @@ export default ({ deleteTweet, tweet }) => {
         right: '14px',
     };
 
+    // No author information in the API besides the 'author' field in tweets, so we're gonna hard-code this badboy
+    const profileImage = () => {
+        switch(tweet.author) {
+            case 'Samantha Geitz':
+                return 'sam.jpg';
+            case 'Keith Damiani':
+                return 'keith.jpg';
+            case 'drumpf':
+                return 'drumpf.jpg';
+            default:
+                return 'default.png';
+        }
+    };
+
     return (
         <ListGroupItem>
-            <h4>
-                { tweet.author }
-                <small className="pull-right">{ moment(tweet.created_at + "-00:00").fromNow() }</small>
-            </h4>
+            <div className="row">
+                <div className="col-md-2">
+                    <img src={`/images/${ profileImage() }`} className="img-thumbnail" height="80px" />
+                </div>
 
-            <p>{ tweet.text }</p>
-            <i className="ion-trash-a" onClick={ () => deleteTweet(tweet.id) } style={ trashButtonStyle } />
+                <div className="col-md-10">
+                    <h4>
+                        { tweet.author }
+                        <small className="pull-right">{ moment(tweet.created_at + "-00:00").fromNow() }</small>
+                    </h4>
+
+                    <p>{ tweet.text }</p>
+                    <i className="ion-trash-a" onClick={ () => deleteTweet(tweet.id) } style={ trashButtonStyle } />
+                </div>
+            </div>
+
         </ListGroupItem>
     );
 }
